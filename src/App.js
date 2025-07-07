d42b20869d6a7553ecee3df1f8865a22e3f251a7
dragos1993 <vrinceanu.dragos24@yahoo.com>
2025-07-07 13:31:47 +0300

    tek

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy.1011
+          Learn Jenkins on Udemy
         </a>
       </header>
       <p>
